a6d4b07a07988b8dfa79cd0d38a7f2ee96e7daf9
Dung <tntgaia2005@gmail.com>
2025-07-05 10:22:36 +0700

    test

---
+++ js/trang1.js
@@ -0,0 +1,13 @@
+function chuyenanh(kq)
+{
+    var anh;
+    if(kq==0)
+    {
+        anh="../img/original-d2952585fbf53cf515bf0da684116e03.webp";
+    }
+    else
+    {
+        anh="../img/Screenshot 2025-07-04 084228.png";
+    }
+    document.getElementById('chuyen').src=anh;
+}
---
+++ js/trang2.js
@@ -0,0 +1,10 @@
+function dangnhap(n)
+{
+    if(n==1)
+    {
+        alert("Dang nhap thanh cong");
+    }
+    else{
+        alert("Dang nhap voi Google");
+        }
+}
